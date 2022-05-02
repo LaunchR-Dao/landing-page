73135c5d28c9b5bc8c38f1929d7d68d189e56628
Andrew Knox <andrewknox@silentk.dev>
2022-05-02 14:52:59 -0700

    changing class to className in Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,12 +29,12 @@ const Navbar = () => {
 
   return (
     <div className="div-container">
-      <nav class="navbar navbar-expand-lg navbar-light ">
-        <a class="navbar-brand" href="#">
+      <nav className="navbar navbar-expand-lg navbar-light ">
+        <a className="navbar-brand" href="#">
           <img src={navlogo} alt="./assets/2.png" height="100px" />
         </a>
         <button
-          class="navbar-toggler"
+          className="navbar-toggler"
           type="button"
           data-toggle="collapse"
           data-target="#navbarSupportedContent"
@@ -42,26 +42,26 @@ const Navbar = () => {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mx-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#" id="nav-item1">
-                Mission<span class="sr-only">(current)</span>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mx-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#" id="nav-item1">
+                Mission<span className="sr-only">(current)</span>
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#" id="nav-item2">
+            <li className="nav-item">
+              <a className="nav-link" href="#" id="nav-item2">
                 RoadMap
               </a>
             </li>
           </ul>
-          <form class="form-inline my-2 my-lg-0">
+          <form className="form-inline my-2 my-lg-0">
             <button
               onClick={requestAccounts}
-              class="btn btn-outline-primary btn-info my-2 my-sm-0"
+              className="btn btn-outline-primary btn-info my-2 my-sm-0"
               type="submit"
               id="nav-button"
             >
