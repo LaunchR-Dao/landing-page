319f97ac314b1788e992741415dba2d6b892f40c
Andrew Knox <andrewknox@silentk.dev>
2022-05-10 21:32:04 -0700

    reconciling asset naming conflicts

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../../assets/youfundr.png";
+import logo from "../../assets/youfundr6.png";
 import "./Footer.css";
 
 export default function Footer() {
