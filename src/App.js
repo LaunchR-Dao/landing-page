07fe9fad6a3335f6953619e0537ab38eee50bc82
Andrew Knox <andrewknox@silentk.dev>
2022-05-16 12:27:35 -0700

    updating folder structure to be consistent across components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Header from "./components/Header";
-import Hero from "./components/Hero/Hero";
+import Hero from "./components/Hero";
 import Mission from "./components/Mission/";
 import About from "./components/About";
 import Roadmap from "./components/Roadmap";
